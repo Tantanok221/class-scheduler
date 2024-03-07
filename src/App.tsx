e2f27d95a37d732d3474b169957f76e327cc6c9c
Tan Kai <6572withspicy@gmail.com>
2024-03-07 21:30:36 +0800

    initialized repo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Calendar from '@toast-ui/react-calendar';
+import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+function App() {
+
+  
+  return (
+    <>
+      <div>
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
