494cac30e6212355c2f275a7791a27a2891882a0
Tan Kai <6572withspicy@gmail.com>
2024-03-07 23:39:44 +0800

    feat: initialized color themes

---
+++ src/theme.css.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@vanilla-extract/css';
+
+export const [themeClass, varThemes] = createTheme({
+  color: {
+    primaryBG: "#151517",
+    secondaryBG: '#262624',
+    white: "#ffffff",
+    primaryMain: "#FB6363",
+    stroke: "#393939"
+  },
+  
+});
