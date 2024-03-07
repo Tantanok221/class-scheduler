36435b3db9080196e78a29e992920147f51bbf94
Tan Kai <6572withspicy@gmail.com>
2024-03-07 23:40:12 +0800

    feat: Initialize day components

---
+++ src/components/Day/Day.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Day = (props: Props) => {
+  return (
+    <div>mon</div>
+  )
+}
+
+export default Day
---
+++ src/components/Day/day.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css';
+import { varThemes } from '../../theme.css';
+
+
+export const dayContainer = style({
+  background : varThemes.color.secondaryBG
+  
+})
